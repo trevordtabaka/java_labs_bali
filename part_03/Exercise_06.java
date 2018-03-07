5efdf015a9937147f2e7a83be329062ebac0517e
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-07 20:36:22 +0800

    part3_exercise 06

--- part_03/Exercise_06.java
+++ part_03/Exercise_06.java
@@ -2,6 +2,7 @@ package part_03;
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 class Exercise_06 {
@@ -18,18 +19,29 @@ class Exercise_06 {
         long totalMilliseconds = System.currentTimeMillis();
 
         // Get total seconds since midnight, 1/1/1970
-
+        long totalSeconds = System.currentTimeMillis()/1000;
         // Get the current second within the minute within the hour
-
+        long currentSecond = totalSeconds%60;
+        System.out.println(currentSecond);
         // Get total minutes
-
+        long totalMinutes = totalSeconds/60;
         // Get the current minute in the hour
+        long currentMinute = totalMinutes%60;
 
         // Get the total hours
-
+        long totalHours = totalMinutes/60;
         // Get the current hour
-
+        long currentHour = totalHours%24;
         // Display results using a 12 hour clock, include AM or PM
+        currentHour = currentHour + timeZoneChange;
+        currentHour = (currentHour > 23) ? currentHour-24: currentHour;
+        currentHour = (currentHour < 0) ? currentHour+24: currentHour;
+        String ampm = (currentHour<12) ? "AM":"PM";
+        currentHour = (currentHour > 12) ? currentHour-12: currentHour;
+
+        String leadingZeroMin = (currentMinute < 10) ? "0" : "";
+        String leadingZeroSec = (currentSecond < 10) ? "0" : "";
+        System.out.println(currentHour + ":" + leadingZeroMin +currentMinute + ":" + leadingZeroSec + currentSecond + " " + ampm);
 
     }
 }
