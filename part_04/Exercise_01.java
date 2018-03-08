7e0c27ae44e308a40f99440a2550b79d651c63db
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-08 11:16:50 +0800

    part4 ex 1

--- part_04/Exercise_01.java
+++ part_04/Exercise_01.java
@@ -15,3 +15,24 @@ import java.util.Scanner;
  Output can be on one line or many lines. However you like.
 
  */
+class Exercise01{
+    public static void main(String[] args) {
+        System.out.println("Enter 1 by 1, 10 integers");
+        Scanner scanner = new Scanner(System.in);
+        int[] intArray = new int[10];
+        for(int i = 0; i<10;i++){
+            int input = scanner.nextInt();
+            intArray[i] = input;
+        }
+
+        for(int i = 1; i<10;i+=2){
+            System.out.print(intArray[i] + ",");
+        }
+        for(int i = 8; i>=0;i-=2){
+            System.out.print(intArray[i] + ",");
+        }
+
+    }
+
+
+}
