ec30f60aa82ee918b2d6cf7778a28efb5abc94cf
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-08 12:05:28 +0800

    part4 ex 4

--- part_04/Exercise_04.java
+++ part_04/Exercise_04.java
@@ -13,12 +13,52 @@ class Exercise_04 {
                 {18, -456, 14, 6, 48, -80}
         };
         // uncomment the lines below when your methods are ready
-//        System.out.println(getMaxVal(dataArray));
-//        System.out.println(getMinVal(dataArray));
+
+
+        System.out.println(getMaxVal(dataArray));
+        System.out.println(getMinVal(dataArray));
+
+
+
     }
 
     // write a method called getMaxVal that takes in a 2D array and iterates through the array to determine the max value in array
 
-    // write a method called getMinVal that takes in a 2D array and iterates through the array to determine the min value in array
 
+    static int getMaxVal(int[][] myArray) {
+        int max = 0;
+        boolean maxIsSet = false;
+        for (int row[] : myArray) {
+            for (int val : row) {
+                if (maxIsSet == false) {
+                    max = val;
+                    maxIsSet = true;
+                } else if (val > max) {
+                    max = val;
+                }
+
+            }
+
+        }
+        return max;
+    }
+
+    // write a method called getMinVal that takes in a 2D array and iterates through the array to determine the min value in array
+    static int getMinVal(int[][] myArray) {
+        int min = 0;
+        boolean minIsSet = false;
+        for (int row[] : myArray) {
+            for (int val : row) {
+                if (minIsSet == false) {
+                    min = val;
+                    minIsSet = true;
+                } else if (val < min) {
+                    min = val;
+                }
+
+            }
+
+        }
+        return min;
+    }
 }
