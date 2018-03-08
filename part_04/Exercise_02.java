fd042ebc6691f61558e5d6ec19d66a7812e92920
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-08 11:24:30 +0800

    part4 ex 2

--- part_04/Exercise_02.java
+++ part_04/Exercise_02.java
@@ -7,3 +7,17 @@ package part_04;
  (ie, 2017, 2015, 2013) of the array in reverse order.
 
  */
+class Exercise_02{
+
+    public static void main(String[] args) {
+        int[] count = new int[2018];
+
+        for(int i = 0; i<2018; i++){
+            count[i] = i;
+        }
+        for(int i = 2017; i>=0; i-=2){
+            System.out.println(count[i]);
+        }
+    }
+
+}
