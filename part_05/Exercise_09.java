a7ddfaebf592ad3333b85dbe25cc40ce2d01c202
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-09 17:12:18 +0800

    part5 ex9

--- part_05/Exercise_09.java
+++ part_05/Exercise_09.java
@@ -6,6 +6,32 @@ package part_05;
  * to the console. Do this at least 3 times with different Strings/sentences.
  */
 
+class Exercise_09{
 
+    public static void main(String[] args) {
+        varArgsString("I", "think", "this", "works", ".");
+        varArgsString("Wow", "it", "did",".");
+        varArgsString("Pretty", "neat", ".");
+    }
+
+
+    static void varArgsString(String ... varargs){
+        StringBuilder myStringBuilder = new StringBuilder();
+        int count = 0;
+        for(String v:varargs){
+            count++;
+            if(varargs.length-1 == count){
+                myStringBuilder.append(v);
+
+            }else{
+                myStringBuilder.append(v + " ");
+
+            }
+
+        }
+        System.out.println(myStringBuilder);
+    }
+
+}
 
 
