18f6057aad0e222f602ef1686cd28ca6366d5122
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-03-14 22:05:06 +0800

    The part 11 exercise 02

--- part_11/Exercise_02.java
+++ part_11/Exercise_02.java
@@ -5,6 +5,29 @@ package part_11;
  */
 
 public class Exercise_02 {
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2,3,4,5};
+        Exercise_01.printArray(intArray);
+        exchangeElement(intArray);
+
+    }
+
+
+    public static <E> void exchangeElement(E[] arr){
+
+        for (int i = 0; i < arr.length/2 ; i++) {
+
+            E temp = arr[arr.length-1-i];
+
+            arr[arr.length-1-i] = arr[i];
+
+            arr[i] = temp;
+
+        }
+        Exercise_01.printArray(arr);
+
+    }
     
 }
 
